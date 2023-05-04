8bd0f152edfffbf1210d1d6e4b8e59b070f1b46d
Weronika Koga <weronika.koga@gmail.com>
2023-05-04 23:02:59 +0200

    prawie wszystkie labele do ExamProtocol

--- Dziedziczenie/Dziedziczenie/ExamProtocol.Designer.cs
+++ Dziedziczenie/Dziedziczenie/ExamProtocol.Designer.cs
@@ -28,12 +28,296 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ExamProtocol";
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            textBox4 = new TextBox();
+            label5 = new Label();
+            textBox5 = new TextBox();
+            label6 = new Label();
+            textBox6 = new TextBox();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            LevelOfStudiesTextBox = new TextBox();
+            FormOfStudiesTextBox = new TextBox();
+            ProfileOfStudyTextBox = new TextBox();
+            AreaOfStudyTextBox = new TextBox();
+            FieldOfStudyTextBox = new TextBox();
+            textBox7 = new TextBox();
+            label10 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(286, 46);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(-3, 108);
+            label2.Name = "label2";
+            label2.Size = new Size(138, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Name and surname";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(141, 101);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(218, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(141, 145);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(218, 27);
+            textBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(36, 145);
+            label3.Name = "label3";
+            label3.Size = new Size(99, 20);
+            label3.TabIndex = 3;
+            label3.Text = "Date of borth";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(141, 246);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(218, 27);
+            textBox3.TabIndex = 8;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(32, 245);
+            label4.Name = "label4";
+            label4.Size = new Size(103, 20);
+            label4.TabIndex = 7;
+            label4.Text = "Student ID No";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(141, 195);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(218, 27);
+            textBox4.TabIndex = 6;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(38, 195);
+            label5.Name = "label5";
+            label5.Size = new Size(97, 20);
+            label5.TabIndex = 5;
+            label5.Text = "Place of birth";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(141, 343);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(218, 27);
+            textBox5.TabIndex = 12;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(44, 314);
+            label6.Name = "label6";
+            label6.Size = new Size(91, 20);
+            label6.TabIndex = 11;
+            label6.Text = "examination";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(141, 294);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(218, 27);
+            textBox6.TabIndex = 10;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(16, 294);
+            label7.Name = "label7";
+            label7.Size = new Size(119, 20);
+            label7.TabIndex = 9;
+            label7.Text = "Date of diploma";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(388, 149);
+            label8.Name = "label8";
+            label8.Size = new Size(97, 20);
+            label8.TabIndex = 36;
+            label8.Text = "Area of study";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(388, 100);
+            label9.Name = "label9";
+            label9.Size = new Size(98, 20);
+            label9.TabIndex = 35;
+            label9.Text = "Field of study";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(383, 301);
+            label11.Name = "label11";
+            label11.Size = new Size(111, 20);
+            label11.TabIndex = 33;
+            label11.Text = "Level of studies";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(385, 252);
+            label12.Name = "label12";
+            label12.Size = new Size(111, 20);
+            label12.TabIndex = 32;
+            label12.Text = "Form of studies";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(385, 201);
+            label13.Name = "label13";
+            label13.Size = new Size(109, 20);
+            label13.TabIndex = 31;
+            label13.Text = "Profile of study";
+            // 
+            // LevelOfStudiesTextBox
+            // 
+            LevelOfStudiesTextBox.Location = new Point(500, 294);
+            LevelOfStudiesTextBox.Name = "LevelOfStudiesTextBox";
+            LevelOfStudiesTextBox.Size = new Size(191, 27);
+            LevelOfStudiesTextBox.TabIndex = 30;
+            // 
+            // FormOfStudiesTextBox
+            // 
+            FormOfStudiesTextBox.Location = new Point(500, 245);
+            FormOfStudiesTextBox.Name = "FormOfStudiesTextBox";
+            FormOfStudiesTextBox.Size = new Size(191, 27);
+            FormOfStudiesTextBox.TabIndex = 29;
+            // 
+            // ProfileOfStudyTextBox
+            // 
+            ProfileOfStudyTextBox.Location = new Point(500, 198);
+            ProfileOfStudyTextBox.Name = "ProfileOfStudyTextBox";
+            ProfileOfStudyTextBox.Size = new Size(191, 27);
+            ProfileOfStudyTextBox.TabIndex = 28;
+            // 
+            // AreaOfStudyTextBox
+            // 
+            AreaOfStudyTextBox.Location = new Point(500, 146);
+            AreaOfStudyTextBox.Name = "AreaOfStudyTextBox";
+            AreaOfStudyTextBox.Size = new Size(191, 27);
+            AreaOfStudyTextBox.TabIndex = 27;
+            // 
+            // FieldOfStudyTextBox
+            // 
+            FieldOfStudyTextBox.Location = new Point(500, 97);
+            FieldOfStudyTextBox.Name = "FieldOfStudyTextBox";
+            FieldOfStudyTextBox.Size = new Size(191, 27);
+            FieldOfStudyTextBox.TabIndex = 26;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(500, 343);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(218, 27);
+            textBox7.TabIndex = 37;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(388, 350);
+            label10.Name = "label10";
+            label10.Size = new Size(98, 20);
+            label10.TabIndex = 38;
+            label10.Text = "Date of study";
+            // 
+            // ExamProtocol
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 1035);
+            Controls.Add(label10);
+            Controls.Add(textBox7);
+            Controls.Add(label8);
+            Controls.Add(label9);
+            Controls.Add(label11);
+            Controls.Add(label12);
+            Controls.Add(label13);
+            Controls.Add(LevelOfStudiesTextBox);
+            Controls.Add(FormOfStudiesTextBox);
+            Controls.Add(ProfileOfStudyTextBox);
+            Controls.Add(AreaOfStudyTextBox);
+            Controls.Add(FieldOfStudyTextBox);
+            Controls.Add(textBox5);
+            Controls.Add(label6);
+            Controls.Add(textBox6);
+            Controls.Add(label7);
+            Controls.Add(textBox3);
+            Controls.Add(label4);
+            Controls.Add(textBox4);
+            Controls.Add(label5);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "ExamProtocol";
+            Text = "ExamProtocol";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox3;
+        private Label label4;
+        private TextBox textBox4;
+        private Label label5;
+        private TextBox textBox5;
+        private Label label6;
+        private TextBox textBox6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private TextBox LevelOfStudiesTextBox;
+        private TextBox FormOfStudiesTextBox;
+        private TextBox ProfileOfStudyTextBox;
+        private TextBox AreaOfStudyTextBox;
+        private TextBox FieldOfStudyTextBox;
+        private TextBox textBox7;
+        private Label label10;
     }
 }
