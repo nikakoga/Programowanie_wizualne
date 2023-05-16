90600cb35d36dee08207cb1f57517b2aabc9faca
Weronika Koga <weronika.koga@gmail.com>
2023-05-16 12:18:48 +0200

    z wiadomoscia

--- ShipsGame/ShipsGame/Form1.cs
+++ ShipsGame/ShipsGame/Form1.cs
@@ -8,7 +8,7 @@ namespace ShipsGame
         public Form1()
         {
             InitializeComponent();
-            //MessageBox.Show("SZANOWNY PANIE DOKTORZE\nWiem, ¿e projekt mia³ mieæ planszê kwadratow¹ i wiem, ¿e udziwniam go dodaj¹c opcjê przegrania,poziom trudnoœci oraz obrazki w miejscach ikonek, ale bardzo chcia³am nauczyæ siê w tym projekcie czegoœ wiêcej i aby moje portfolio na githubie by³o bardziej zaawansowane. Jeœli jest to niezgodne z wymaganiami, mogê cofn¹æ projekt do wersji podstawowej");
+            MessageBox.Show("SZANOWNY PANIE DOKTORZE\nWiem, ¿e projekt mia³ mieæ planszê kwadratow¹ i wiem, ¿e udziwniam go dodaj¹c poziom trudnoœci oraz obrazki w miejscach ikonek, ale bardzo chcia³am nauczyæ siê w tym projekcie czegoœ wiêcej i aby moje portfolio na githubie by³o bardziej zaawansowane. Zrobi³am równie¿ w zaleznoœci od poziomu trudnoœci limit ruchów a nie limit czasowy. Jeœli jest to niezgodne z wymaganiami, mogê cofn¹æ projekt do wersji podstawowej");
         }
 
         private void button1_Click(object sender, EventArgs e)
