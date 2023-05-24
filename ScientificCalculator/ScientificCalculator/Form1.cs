45db045c707e00647a281f8584e62f3f33811cad
Weronika Koga <weronika.koga@gmail.com>
2023-05-24 16:30:30 +0200

    bez trycodu widzialnosci przyciskow

--- ScientificCalculator/ScientificCalculator/Form1.cs
+++ ScientificCalculator/ScientificCalculator/Form1.cs
@@ -2,24 +2,18 @@ namespace ScientificCalculator
 {
     public partial class Form1 : Form
     {
+        string expression;
+        Button[] Scientific;
         public Form1()
         {
             InitializeComponent();
-            btnRad.Visible = false;
-            btnDeg.Visible = false;
-            btnInv.Visible = false;
-            btnPi.Visible = false;
-            btnE.Visible = false;
-            btnAns.Visible = false;
-            btnSin.Visible = false;
-            btnCos.Visible = false;
-            btnTan.Visible = false;
-            btnExp.Visible = false;
-            btnx.Visible = false;
-            btnLn.Visible = false;
-            btnLog.Visible = false;
-            btnPierw.Visible = false;
-            btnxy.Visible = false;
+
+            Scientific = new Button[] {btnRad,btnDeg,btnInv,btnPi,btnE,btnAns,btnSin,btnCos,btnTan,btnExp,btnx,btnLn,btnLog,btnPierw,btnxy };
+
+            foreach (var btn in Scientific)
+            {
+                btn.Visible = false;
+            }
         }
 
         private void btnRow_Click(object sender, EventArgs e)
@@ -31,40 +25,24 @@ namespace ScientificCalculator
         {
             if(checkBox1.Checked)
             {
-                btnRad.Visible = true;
-                btnDeg.Visible = true;
-                btnInv.Visible = true;
-                btnPi.Visible = true;
-                btnE.Visible = true;
-                btnAns.Visible = true;
-                btnSin.Visible = true;
-                btnCos.Visible = true;
-                btnTan.Visible = true;
-                btnExp.Visible = true;
-                btnx.Visible = true;
-                btnLn.Visible = true;
-                btnLog.Visible = true;
-                btnPierw.Visible = true;
-                btnxy.Visible = true;
+                foreach (var btn in Scientific)
+                {
+                    btn.Visible = true;
+                }
             }
             else
             {
-                btnRad.Visible = false;
-                btnDeg.Visible = false;
-                btnInv.Visible = false;
-                btnPi.Visible = false;
-                btnE.Visible = false;
-                btnAns.Visible = false;
-                btnSin.Visible = false;
-                btnCos.Visible = false;
-                btnTan.Visible = false;
-                btnExp.Visible = false;
-                btnx.Visible = false;
-                btnLn.Visible = false;
-                btnLog.Visible = false;
-                btnPierw.Visible = false;
-                btnxy.Visible = false;
+                foreach (var btn in Scientific)
+                {
+                    btn.Visible = false;
+                }
             }
+        }
+
+        void Button_Click(object sender, EventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            expression += clickedButton.Text;
         }
     }
 }
