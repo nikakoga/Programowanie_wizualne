b962f1f3432df468580cba9b48d9eaf981759bc1
Weronika Koga <weronika.koga@gmail.com>
2023-05-24 16:11:26 +0200

    zmiana widocznosci

--- ScientificCalculator/ScientificCalculator/Form1.Designer.cs
+++ ScientificCalculator/ScientificCalculator/Form1.Designer.cs
@@ -28,12 +28,507 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            tbxWindow = new TextBox();
+            listView1 = new ListView();
+            lblHistory = new Label();
+            btnLn = new Button();
+            btn7 = new Button();
+            btn8 = new Button();
+            btn5 = new Button();
+            btn4 = new Button();
+            btnLog = new Button();
+            btn2 = new Button();
+            btn1 = new Button();
+            btnPierw = new Button();
+            btnKrop = new Button();
+            btn0 = new Button();
+            btnxy = new Button();
+            btnRow = new Button();
+            btn3 = new Button();
+            btn6 = new Button();
+            btn9 = new Button();
+            btnProc = new Button();
+            btnNawZa = new Button();
+            btnNawO = new Button();
+            btnx = new Button();
+            btnDeg = new Button();
+            btnRad = new Button();
+            btnExp = new Button();
+            btnAns = new Button();
+            btnTan = new Button();
+            btnE = new Button();
+            btnCos = new Button();
+            btnPi = new Button();
+            btnSin = new Button();
+            btnInv = new Button();
+            btnAc = new Button();
+            btnDod = new Button();
+            btnOdj = new Button();
+            btnMnoz = new Button();
+            btnDziel = new Button();
+            label1 = new Label();
+            checkBox1 = new CheckBox();
+            SuspendLayout();
+            // 
+            // tbxWindow
+            // 
+            tbxWindow.Location = new Point(248, 72);
+            tbxWindow.Name = "tbxWindow";
+            tbxWindow.Size = new Size(505, 27);
+            tbxWindow.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(12, 72);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(219, 271);
+            listView1.TabIndex = 1;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // lblHistory
+            // 
+            lblHistory.AutoSize = true;
+            lblHistory.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            lblHistory.ForeColor = SystemColors.MenuHighlight;
+            lblHistory.Location = new Point(82, 44);
+            lblHistory.Name = "lblHistory";
+            lblHistory.Size = new Size(59, 20);
+            lblHistory.TabIndex = 2;
+            lblHistory.Text = "History";
+            // 
+            // btnLn
+            // 
+            btnLn.Location = new Point(394, 161);
+            btnLn.Name = "btnLn";
+            btnLn.Size = new Size(67, 41);
+            btnLn.TabIndex = 3;
+            btnLn.Text = "ln";
+            btnLn.UseVisualStyleBackColor = true;
+            // 
+            // btn7
+            // 
+            btn7.Location = new Point(467, 161);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(67, 41);
+            btn7.TabIndex = 4;
+            btn7.Text = "7";
+            btn7.UseVisualStyleBackColor = true;
+            // 
+            // btn8
+            // 
+            btn8.Location = new Point(540, 161);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(67, 41);
+            btn8.TabIndex = 5;
+            btn8.Text = "8";
+            btn8.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            btn5.Location = new Point(540, 208);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(67, 41);
+            btn5.TabIndex = 8;
+            btn5.Text = "5";
+            btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            btn4.Location = new Point(467, 208);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(67, 41);
+            btn4.TabIndex = 7;
+            btn4.Text = "4";
+            btn4.UseVisualStyleBackColor = true;
+            // 
+            // btnLog
+            // 
+            btnLog.Location = new Point(394, 208);
+            btnLog.Name = "btnLog";
+            btnLog.Size = new Size(67, 41);
+            btnLog.TabIndex = 6;
+            btnLog.Text = "log";
+            btnLog.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            btn2.Location = new Point(540, 255);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(67, 41);
+            btn2.TabIndex = 11;
+            btn2.Text = "2";
+            btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn1
+            // 
+            btn1.Location = new Point(467, 255);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(67, 41);
+            btn1.TabIndex = 10;
+            btn1.Text = "1";
+            btn1.UseVisualStyleBackColor = true;
+            // 
+            // btnPierw
+            // 
+            btnPierw.Location = new Point(394, 255);
+            btnPierw.Name = "btnPierw";
+            btnPierw.Size = new Size(67, 41);
+            btnPierw.TabIndex = 9;
+            btnPierw.Text = "√ ";
+            btnPierw.UseVisualStyleBackColor = true;
+            // 
+            // btnKrop
+            // 
+            btnKrop.Location = new Point(540, 302);
+            btnKrop.Name = "btnKrop";
+            btnKrop.Size = new Size(67, 41);
+            btnKrop.TabIndex = 14;
+            btnKrop.Text = ".";
+            btnKrop.UseVisualStyleBackColor = true;
+            // 
+            // btn0
+            // 
+            btn0.Location = new Point(467, 302);
+            btn0.Name = "btn0";
+            btn0.Size = new Size(67, 41);
+            btn0.TabIndex = 13;
+            btn0.Text = "0";
+            btn0.UseVisualStyleBackColor = true;
+            // 
+            // btnxy
+            // 
+            btnxy.Location = new Point(394, 302);
+            btnxy.Name = "btnxy";
+            btnxy.Size = new Size(67, 41);
+            btnxy.TabIndex = 12;
+            btnxy.Text = "x^y";
+            btnxy.UseVisualStyleBackColor = true;
+            // 
+            // btnRow
+            // 
+            btnRow.BackColor = SystemColors.ActiveCaption;
+            btnRow.Location = new Point(613, 302);
+            btnRow.Name = "btnRow";
+            btnRow.Size = new Size(67, 41);
+            btnRow.TabIndex = 18;
+            btnRow.Text = "=";
+            btnRow.UseVisualStyleBackColor = false;
+            btnRow.Click += btnRow_Click;
+            // 
+            // btn3
+            // 
+            btn3.Location = new Point(613, 255);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(67, 41);
+            btn3.TabIndex = 17;
+            btn3.Text = "3";
+            btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            btn6.Location = new Point(613, 208);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(67, 41);
+            btn6.TabIndex = 16;
+            btn6.Text = "6";
+            btn6.UseVisualStyleBackColor = true;
+            // 
+            // btn9
+            // 
+            btn9.Location = new Point(613, 161);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(67, 41);
+            btn9.TabIndex = 15;
+            btn9.Text = "9";
+            btn9.UseVisualStyleBackColor = true;
+            // 
+            // btnProc
+            // 
+            btnProc.Location = new Point(613, 114);
+            btnProc.Name = "btnProc";
+            btnProc.Size = new Size(67, 41);
+            btnProc.TabIndex = 22;
+            btnProc.Text = "%";
+            btnProc.UseVisualStyleBackColor = true;
+            // 
+            // btnNawZa
+            // 
+            btnNawZa.Location = new Point(540, 114);
+            btnNawZa.Name = "btnNawZa";
+            btnNawZa.Size = new Size(67, 41);
+            btnNawZa.TabIndex = 21;
+            btnNawZa.Text = ")";
+            btnNawZa.UseVisualStyleBackColor = true;
+            // 
+            // btnNawO
+            // 
+            btnNawO.Location = new Point(467, 114);
+            btnNawO.Name = "btnNawO";
+            btnNawO.Size = new Size(67, 41);
+            btnNawO.TabIndex = 20;
+            btnNawO.Text = "(";
+            btnNawO.UseVisualStyleBackColor = true;
+            // 
+            // btnx
+            // 
+            btnx.Location = new Point(394, 114);
+            btnx.Name = "btnx";
+            btnx.Size = new Size(67, 41);
+            btnx.TabIndex = 19;
+            btnx.Text = "x!";
+            btnx.UseVisualStyleBackColor = true;
+            // 
+            // btnDeg
+            // 
+            btnDeg.Location = new Point(321, 114);
+            btnDeg.Name = "btnDeg";
+            btnDeg.Size = new Size(67, 41);
+            btnDeg.TabIndex = 32;
+            btnDeg.Text = "Deg";
+            btnDeg.UseVisualStyleBackColor = true;
+            // 
+            // btnRad
+            // 
+            btnRad.Location = new Point(248, 114);
+            btnRad.Name = "btnRad";
+            btnRad.Size = new Size(67, 41);
+            btnRad.TabIndex = 31;
+            btnRad.Text = "Rad";
+            btnRad.UseVisualStyleBackColor = true;
+            // 
+            // btnExp
+            // 
+            btnExp.Location = new Point(321, 302);
+            btnExp.Name = "btnExp";
+            btnExp.Size = new Size(67, 41);
+            btnExp.TabIndex = 30;
+            btnExp.Text = "EXP";
+            btnExp.UseVisualStyleBackColor = true;
+            // 
+            // btnAns
+            // 
+            btnAns.Location = new Point(248, 302);
+            btnAns.Name = "btnAns";
+            btnAns.Size = new Size(67, 41);
+            btnAns.TabIndex = 29;
+            btnAns.Text = "Ans";
+            btnAns.UseVisualStyleBackColor = true;
+            // 
+            // btnTan
+            // 
+            btnTan.Location = new Point(321, 255);
+            btnTan.Name = "btnTan";
+            btnTan.Size = new Size(67, 41);
+            btnTan.TabIndex = 28;
+            btnTan.Text = "tan";
+            btnTan.UseVisualStyleBackColor = true;
+            // 
+            // btnE
+            // 
+            btnE.Location = new Point(248, 255);
+            btnE.Name = "btnE";
+            btnE.Size = new Size(67, 41);
+            btnE.TabIndex = 27;
+            btnE.Text = "e";
+            btnE.UseVisualStyleBackColor = true;
+            // 
+            // btnCos
+            // 
+            btnCos.Location = new Point(321, 208);
+            btnCos.Name = "btnCos";
+            btnCos.Size = new Size(67, 41);
+            btnCos.TabIndex = 26;
+            btnCos.Text = "cos";
+            btnCos.UseVisualStyleBackColor = true;
+            // 
+            // btnPi
+            // 
+            btnPi.Location = new Point(248, 208);
+            btnPi.Name = "btnPi";
+            btnPi.Size = new Size(67, 41);
+            btnPi.TabIndex = 25;
+            btnPi.Text = "π";
+            btnPi.UseVisualStyleBackColor = true;
+            // 
+            // btnSin
+            // 
+            btnSin.Location = new Point(321, 161);
+            btnSin.Name = "btnSin";
+            btnSin.Size = new Size(67, 41);
+            btnSin.TabIndex = 24;
+            btnSin.Text = "sin";
+            btnSin.UseVisualStyleBackColor = true;
+            // 
+            // btnInv
+            // 
+            btnInv.Location = new Point(248, 161);
+            btnInv.Name = "btnInv";
+            btnInv.Size = new Size(67, 41);
+            btnInv.TabIndex = 23;
+            btnInv.Text = "Inv";
+            btnInv.UseVisualStyleBackColor = true;
+            // 
+            // btnAc
+            // 
+            btnAc.Location = new Point(686, 114);
+            btnAc.Name = "btnAc";
+            btnAc.Size = new Size(67, 41);
+            btnAc.TabIndex = 37;
+            btnAc.Text = "AC";
+            btnAc.UseVisualStyleBackColor = true;
+            // 
+            // btnDod
+            // 
+            btnDod.Location = new Point(686, 302);
+            btnDod.Name = "btnDod";
+            btnDod.Size = new Size(67, 41);
+            btnDod.TabIndex = 36;
+            btnDod.Text = "+";
+            btnDod.UseVisualStyleBackColor = true;
+            // 
+            // btnOdj
+            // 
+            btnOdj.Location = new Point(686, 255);
+            btnOdj.Name = "btnOdj";
+            btnOdj.Size = new Size(67, 41);
+            btnOdj.TabIndex = 35;
+            btnOdj.Text = "-";
+            btnOdj.UseVisualStyleBackColor = true;
+            // 
+            // btnMnoz
+            // 
+            btnMnoz.Location = new Point(686, 208);
+            btnMnoz.Name = "btnMnoz";
+            btnMnoz.Size = new Size(67, 41);
+            btnMnoz.TabIndex = 34;
+            btnMnoz.Text = "x";
+            btnMnoz.UseVisualStyleBackColor = true;
+            // 
+            // btnDziel
+            // 
+            btnDziel.Location = new Point(686, 161);
+            btnDziel.Name = "btnDziel";
+            btnDziel.Size = new Size(67, 41);
+            btnDziel.TabIndex = 33;
+            btnDziel.Text = "÷";
+            btnDziel.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.MenuHighlight;
+            label1.Location = new Point(703, 49);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 38;
+            label1.Text = "Result";
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            checkBox1.ForeColor = SystemColors.MenuHighlight;
+            checkBox1.Location = new Point(259, 22);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(92, 24);
+            checkBox1.TabIndex = 39;
+            checkBox1.Text = "Scientific";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(806, 394);
+            Controls.Add(checkBox1);
+            Controls.Add(label1);
+            Controls.Add(btnAc);
+            Controls.Add(btnDod);
+            Controls.Add(btnOdj);
+            Controls.Add(btnMnoz);
+            Controls.Add(btnDziel);
+            Controls.Add(btnDeg);
+            Controls.Add(btnRad);
+            Controls.Add(btnExp);
+            Controls.Add(btnAns);
+            Controls.Add(btnTan);
+            Controls.Add(btnE);
+            Controls.Add(btnCos);
+            Controls.Add(btnPi);
+            Controls.Add(btnSin);
+            Controls.Add(btnInv);
+            Controls.Add(btnProc);
+            Controls.Add(btnNawZa);
+            Controls.Add(btnNawO);
+            Controls.Add(btnx);
+            Controls.Add(btnRow);
+            Controls.Add(btn3);
+            Controls.Add(btn6);
+            Controls.Add(btn9);
+            Controls.Add(btnKrop);
+            Controls.Add(btn0);
+            Controls.Add(btnxy);
+            Controls.Add(btn2);
+            Controls.Add(btn1);
+            Controls.Add(btnPierw);
+            Controls.Add(btn5);
+            Controls.Add(btn4);
+            Controls.Add(btnLog);
+            Controls.Add(btn8);
+            Controls.Add(btn7);
+            Controls.Add(btnLn);
+            Controls.Add(lblHistory);
+            Controls.Add(listView1);
+            Controls.Add(tbxWindow);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox tbxWindow;
+        private ListView listView1;
+        private Label lblHistory;
+        private Button btnLn;
+        private Button btn7;
+        private Button btn8;
+        private Button btn5;
+        private Button btn4;
+        private Button btnLog;
+        private Button btn2;
+        private Button btn1;
+        private Button btnPierw;
+        private Button btnKrop;
+        private Button btn0;
+        private Button btnxy;
+        private Button btnRow;
+        private Button btn3;
+        private Button btn6;
+        private Button btn9;
+        private Button btnProc;
+        private Button btnNawZa;
+        private Button btnNawO;
+        private Button btnx;
+        private Button btnDeg;
+        private Button btnRad;
+        private Button btnExp;
+        private Button btnAns;
+        private Button btnTan;
+        private Button btnE;
+        private Button btnCos;
+        private Button btnPi;
+        private Button btnSin;
+        private Button btnInv;
+        private Button btnAc;
+        private Button btnDod;
+        private Button btnOdj;
+        private Button btnMnoz;
+        private Button btnDziel;
+        private Label label1;
+        private CheckBox checkBox1;
     }
 }
--- ScientificCalculator/ScientificCalculator/Form1.cs
+++ ScientificCalculator/ScientificCalculator/Form1.cs
@@ -5,6 +5,66 @@ namespace ScientificCalculator
         public Form1()
         {
             InitializeComponent();
+            btnRad.Visible = false;
+            btnDeg.Visible = false;
+            btnInv.Visible = false;
+            btnPi.Visible = false;
+            btnE.Visible = false;
+            btnAns.Visible = false;
+            btnSin.Visible = false;
+            btnCos.Visible = false;
+            btnTan.Visible = false;
+            btnExp.Visible = false;
+            btnx.Visible = false;
+            btnLn.Visible = false;
+            btnLog.Visible = false;
+            btnPierw.Visible = false;
+            btnxy.Visible = false;
+        }
+
+        private void btnRow_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked)
+            {
+                btnRad.Visible = true;
+                btnDeg.Visible = true;
+                btnInv.Visible = true;
+                btnPi.Visible = true;
+                btnE.Visible = true;
+                btnAns.Visible = true;
+                btnSin.Visible = true;
+                btnCos.Visible = true;
+                btnTan.Visible = true;
+                btnExp.Visible = true;
+                btnx.Visible = true;
+                btnLn.Visible = true;
+                btnLog.Visible = true;
+                btnPierw.Visible = true;
+                btnxy.Visible = true;
+            }
+            else
+            {
+                btnRad.Visible = false;
+                btnDeg.Visible = false;
+                btnInv.Visible = false;
+                btnPi.Visible = false;
+                btnE.Visible = false;
+                btnAns.Visible = false;
+                btnSin.Visible = false;
+                btnCos.Visible = false;
+                btnTan.Visible = false;
+                btnExp.Visible = false;
+                btnx.Visible = false;
+                btnLn.Visible = false;
+                btnLog.Visible = false;
+                btnPierw.Visible = false;
+                btnxy.Visible = false;
+            }
         }
     }
 }
