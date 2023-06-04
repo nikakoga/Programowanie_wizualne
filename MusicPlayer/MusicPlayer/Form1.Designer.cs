f7cc53c86820c4ceb1ad81e57454bf234777e10b
Weronika Koga <weronika.koga@gmail.com>
2023-06-04 14:51:08 +0200

    przyciski i labelki

--- MusicPlayer/MusicPlayer/Form1.Designer.cs
+++ MusicPlayer/MusicPlayer/Form1.Designer.cs
@@ -28,81 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            listBox1 = new ListBox();
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
-            button4 = new Button();
-            button5 = new Button();
+            lbxPlaylist = new ListBox();
+            BtnAdd = new Button();
+            BtnDelete = new Button();
+            BtnPause = new Button();
+            BtnPlay = new Button();
+            BtnStop = new Button();
             trackBar1 = new TrackBar();
+            label1 = new Label();
             ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
             SuspendLayout();
             // 
-            // listBox1
+            // lbxPlaylist
             // 
-            listBox1.BackColor = Color.FromArgb(31, 30, 68);
-            listBox1.BorderStyle = BorderStyle.None;
-            listBox1.ForeColor = Color.Red;
-            listBox1.FormattingEnabled = true;
-            listBox1.ItemHeight = 20;
-            listBox1.Location = new Point(2, 3);
-            listBox1.Name = "listBox1";
-            listBox1.Size = new Size(223, 400);
-            listBox1.TabIndex = 0;
-            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            lbxPlaylist.BackColor = Color.FromArgb(31, 30, 68);
+            lbxPlaylist.BorderStyle = BorderStyle.None;
+            lbxPlaylist.ForeColor = Color.Red;
+            lbxPlaylist.FormattingEnabled = true;
+            lbxPlaylist.ItemHeight = 20;
+            lbxPlaylist.Location = new Point(-1, 110);
+            lbxPlaylist.Name = "lbxPlaylist";
+            lbxPlaylist.Size = new Size(362, 340);
+            lbxPlaylist.TabIndex = 0;
+            lbxPlaylist.SelectedIndexChanged += listBox1_SelectedIndexChanged;
             // 
-            // button1
+            // BtnAdd
             // 
-            button1.Location = new Point(22, 409);
-            button1.Name = "button1";
-            button1.Size = new Size(94, 29);
-            button1.TabIndex = 1;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            BtnAdd.Font = new Font("Showcard Gothic", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            BtnAdd.ForeColor = Color.Red;
+            BtnAdd.Location = new Point(17, 61);
+            BtnAdd.Name = "BtnAdd";
+            BtnAdd.Size = new Size(53, 43);
+            BtnAdd.TabIndex = 1;
+            BtnAdd.Text = "+";
+            BtnAdd.UseVisualStyleBackColor = true;
+            BtnAdd.Click += BtnAdd_Click;
             // 
-            // button2
+            // BtnDelete
             // 
-            button2.Location = new Point(122, 409);
-            button2.Name = "button2";
-            button2.Size = new Size(94, 29);
-            button2.TabIndex = 2;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
+            BtnDelete.Font = new Font("Showcard Gothic", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            BtnDelete.ForeColor = Color.Red;
+            BtnDelete.Location = new Point(85, 61);
+            BtnDelete.Name = "BtnDelete";
+            BtnDelete.Size = new Size(47, 43);
+            BtnDelete.TabIndex = 2;
+            BtnDelete.Text = "-";
+            BtnDelete.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // BtnPause
             // 
-            button3.Location = new Point(489, 192);
-            button3.Name = "button3";
-            button3.Size = new Size(94, 29);
-            button3.TabIndex = 4;
-            button3.Text = "button3";
-            button3.UseVisualStyleBackColor = true;
+            BtnPause.ForeColor = SystemColors.ControlText;
+            BtnPause.Image = Properties.Resources.iconPause;
+            BtnPause.Location = new Point(516, 188);
+            BtnPause.Name = "BtnPause";
+            BtnPause.Size = new Size(57, 40);
+            BtnPause.TabIndex = 4;
+            BtnPause.UseVisualStyleBackColor = true;
+            BtnPause.Click += BtnPause_Click;
             // 
-            // button4
+            // BtnPlay
             // 
-            button4.Location = new Point(389, 192);
-            button4.Name = "button4";
-            button4.Size = new Size(94, 29);
-            button4.TabIndex = 3;
-            button4.Text = "button4";
-            button4.UseVisualStyleBackColor = true;
+            BtnPlay.Image = Properties.Resources.iconPlay;
+            BtnPlay.Location = new Point(458, 188);
+            BtnPlay.Name = "BtnPlay";
+            BtnPlay.Size = new Size(52, 40);
+            BtnPlay.TabIndex = 3;
+            BtnPlay.UseVisualStyleBackColor = true;
+            BtnPlay.Click += BtnPlay_Click;
             // 
-            // button5
+            // BtnStop
             // 
-            button5.Location = new Point(589, 192);
-            button5.Name = "button5";
-            button5.Size = new Size(94, 29);
-            button5.TabIndex = 5;
-            button5.Text = "button5";
-            button5.UseVisualStyleBackColor = true;
+            BtnStop.Image = Properties.Resources.iconStop;
+            BtnStop.Location = new Point(579, 188);
+            BtnStop.Name = "BtnStop";
+            BtnStop.Size = new Size(55, 40);
+            BtnStop.TabIndex = 5;
+            BtnStop.UseVisualStyleBackColor = true;
+            BtnStop.Click += BtnStop_Click;
             // 
             // trackBar1
             // 
-            trackBar1.Location = new Point(389, 115);
+            trackBar1.Location = new Point(406, 126);
             trackBar1.Name = "trackBar1";
             trackBar1.Size = new Size(294, 56);
             trackBar1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Showcard Gothic", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = Color.Red;
+            label1.Location = new Point(17, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(116, 29);
+            label1.TabIndex = 7;
+            label1.Text = "Playlist";
             // 
             // Form1
             // 
@@ -110,13 +130,14 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(31, 30, 68);
             ClientSize = new Size(800, 450);
+            Controls.Add(label1);
             Controls.Add(trackBar1);
-            Controls.Add(button5);
-            Controls.Add(button3);
-            Controls.Add(button4);
-            Controls.Add(button2);
-            Controls.Add(button1);
-            Controls.Add(listBox1);
+            Controls.Add(BtnStop);
+            Controls.Add(BtnPause);
+            Controls.Add(BtnPlay);
+            Controls.Add(BtnDelete);
+            Controls.Add(BtnAdd);
+            Controls.Add(lbxPlaylist);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
@@ -127,12 +148,13 @@
 
         #endregion
 
-        private ListBox listBox1;
-        private Button button1;
-        private Button button2;
-        private Button button3;
-        private Button button4;
-        private Button button5;
+        private ListBox lbxPlaylist;
+        private Button BtnAdd;
+        private Button BtnDelete;
+        private Button BtnPause;
+        private Button BtnPlay;
+        private Button BtnStop;
         private TrackBar trackBar1;
+        private Label label1;
     }
 }
--- MusicPlayer/MusicPlayer/Form1.cs
+++ MusicPlayer/MusicPlayer/Form1.cs
@@ -1,12 +1,14 @@
 namespace MusicPlayer
 {
+    using System.Linq.Expressions;
     using WMPLib;
     public partial class Form1 : Form
     {
-        WMPLib.WindowsMediaPlayer plzyer = new WMPLib.WindowsMediaPlayer();
+        WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer();
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -16,10 +18,10 @@ namespace MusicPlayer
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void BtnAdd_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
             ofd.Filter = "MP3| *.mp3";
@@ -28,8 +30,25 @@ namespace MusicPlayer
             if (ofd.ShowDialog() == DialogResult.OK)
             {
                 string path_open = ofd.FileName;
-                string extension = Path.GetExtension(ofd.FileName).ToLower();
+                string filename = Path.GetFileName(path_open);
+                lbxPlaylist.Items.Insert(0, filename);
+                player.URL = path_open;
             }
+        }
+
+        private void BtnPlay_Click(object sender, EventArgs e)
+        {
+            player.controls.play();
+        }
+
+        private void BtnPause_Click(object sender, EventArgs e)
+        {
+            player.controls.pause();
+        }
+
+        private void BtnStop_Click(object sender, EventArgs e)
+        {
+            player.controls.stop();
         }
     }
 }
